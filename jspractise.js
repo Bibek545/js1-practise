d9fcea4bd4195a1b681288b8ee69bc497605918a
Bibek Hanal <bbekhamal26@gmail.com>
2025-03-05 14:33:07 +1100

    new class BanKAccount

--- jspractise.js
+++ jspractise.js
@@ -44,3 +44,53 @@ reverse();
     }
     console.log(total);
 
+
+
+    class BankAccount {
+        constructor(accountNumber, accountHolder, name, balance) {
+            this.accountNumber = accountNumber;
+            // this.accountHolder = accountHolder;
+            this.name = name;
+            this.balance = balance;
+        }
+        deposit(amount) {
+            this.balance += amount;
+            console.log(`'Deposited amount is: ' ${amount}`);
+
+        }
+        withdraw(amount) {
+            // conditon here if not enough money
+            if (amount > this.balance  ) {
+                console.log('Not sufficient money')
+                return false;
+            } else {
+                this.balance -= amount;
+                return true;
+            }            
+            console.log(`'Withdrawn amount is: ' ${amount}`);
+        }
+        transferMoney(personobj, amount) {
+            //1. withdraw from the account
+            if (this.withdraw(amount)) {
+
+            
+            //2. deposit into new account
+            personobj.deposit(amount); 
+            } else {
+                console.log('Transfer unsuccessful.')
+            }
+        }
+    }
+
+    // new_account = BankAccount("Bibek", 123456, $5000 )
+    //creating an instances
+   const person1 = new   BankAccount("Bibek", 123456, 5000 );
+   const person2 = new   BankAccount("Hamal", 23456, 8000 );
+
+    
+   console.log(person1, person2);
+   console.log(person2.withdraw(500));
+   console.log(person2.transferMoney(person1, 500));
+
+
+
