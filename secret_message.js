eea23c2193af9a59bf94a8e85008f480fd9a4722
Bibek Hanal <bbekhamal26@gmail.com>
2025-03-16 22:36:03 +1100

    added new js file for secret message

---
+++ secret_message.js
@@ -0,0 +1,16 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length);
+secretMessage.pop();
+// console.log(secretMessage);
+console.log(secretMessage.length);
+secretMessage.push('to', 'program');
+// console.log(secretMessage);
+secretMessage[7] = 'right';
+// console.log(secretMessage);
+let firstElement = secretMessage.shift();
+// console.log(secretMessage);
+secretMessage[0] = 'Programming';
+console.log(secretMessage);
+secretMessage.splice(6, 5, 'know');
+console.log(secretMessage);
+console.log(secretMessage.join(' '));
